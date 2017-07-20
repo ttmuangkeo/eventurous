628b529c3fb47e68ad4546151a24ede69fe2db32
ttmuangkeo <tim.muangkeo@gmail.com>
2017-07-20 13:28:34 -0700

    updated homepage

--- public/script.js
+++ public/script.js
@@ -3,8 +3,88 @@ var initMap = function() {
 
     var map = new google.maps.Map(document.getElementById('map-container'), {
         center: { lat: -34.397, lng: 150.644 },
-        zoom: 10
-    });
+        zoom: 10,
+        styles: [
+            {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+            {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+            {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+            {
+              featureType: 'administrative.locality',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'poi',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'poi.park',
+              elementType: 'geometry',
+              stylers: [{color: '#263c3f'}]
+            },
+            {
+              featureType: 'poi.park',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#6b9a76'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'geometry',
+              stylers: [{color: '#38414e'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'geometry.stroke',
+              stylers: [{color: '#212a37'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#9ca5b3'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'geometry',
+              stylers: [{color: '#746855'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'geometry.stroke',
+              stylers: [{color: '#1f2835'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#f3d19c'}]
+            },
+            {
+              featureType: 'transit',
+              elementType: 'geometry',
+              stylers: [{color: '#2f3948'}]
+            },
+            {
+              featureType: 'transit.station',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'geometry',
+              stylers: [{color: '#17263c'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#515c6d'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'labels.text.stroke',
+              stylers: [{color: '#17263c'}]
+            }
+          ]
+        });
 
     // if brower support available, ask user for location data and set the map view
     if (navigator.geolocation) {
@@ -12,7 +92,7 @@ var initMap = function() {
             var initialLocation = new google.maps.LatLng(
                 position.coords.latitude,
                 position.coords.longitude
-            );
+                );
             map.setCenter(initialLocation);
         });
     }
@@ -38,30 +118,30 @@ var initMap = function() {
         });
     });
 
-    $('.edit-form').submit(function(e) {
-        e.preventDefault();
-        var url = $(this).attr('action');
-        var data = $(this).serialize();
+}
 
-        $.ajax({
-            url: url,
-            method: 'PUT',
-            data: data
-        }).done(function() {
-            window.location.href = '/profile';
-        });
-    });
+// $('.edit-form').submit(function(e) {
+//     e.preventDefault();
+//     var url = $(this).attr('action');
+//     var data = $(this).serialize();
 
-    $('.delete-btn').click(function(e) {
-        e.preventDefault();
-        var url = $(this).attr('href');
+//     $.ajax({
+//         url: url,
+//         method: 'PUT',
+//         data: data
+//     }).done(function() {
+//         window.location.href = '/profile';
+//     });
+// });
 
-        $.ajax({
-            url: url,
-            method: 'DELETE'
-        }).done(function() {
-            window.location.href = '/profile';
-        });
-    });
+// $('.delete-btn').click(function(e) {
+//     e.preventDefault();
+//     var url = $(this).attr('href');
 
-};
+//     $.ajax({
+//         url: url,
+//         method: 'DELETE'
+//     }).done(function() {
+//         window.location.href = '/profile';
+//     });
+// });
